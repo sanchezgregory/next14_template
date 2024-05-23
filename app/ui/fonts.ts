75053238ebf9010ab02724fcd98195a4aa361ccf
Gregory Sanchez <gregory@dev.com>
2024-05-23 13:20:31 -0300

    Using a new particular font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import {Montserrat} from 'next/font/google'
+import {Montserrat, Damion} from 'next/font/google'
 
 export const montserrat = Montserrat({subsets:['latin'], weight: '700'})
+
+export const damion = Damion({weight:"400", subsets:['latin']})
